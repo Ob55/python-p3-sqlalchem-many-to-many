79d0346220dd8bd85d1a44cb7668ef0c92c51516
Ob55 <brian55mwangi@gmail.com>
2024-03-23 14:30:38 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
+
+
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
